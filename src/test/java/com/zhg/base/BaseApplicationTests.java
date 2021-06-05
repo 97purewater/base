8960a3b60b3a02da102bf234b4728916e52df1a1
97purewater <379633105@qq.com>
2021-06-05 22:10:29 +0800

    demo
    Signed-off-by: 97purewater <379633105@qq.com>

---
+++ src/test/java/com/zhg/base/BaseApplicationTests.java
@@ -0,0 +1,14 @@
+package com.zhg.base;
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
